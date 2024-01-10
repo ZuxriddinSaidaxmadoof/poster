492ce420d9181d1d0bf9b5d1adca67564cf769ab
Zuxriddin <149867184+ZuxriddinSaidaxmadoof@users.noreply.github.com>
2024-01-10 22:22:37 +0500

    starting tasks

---
+++ src/routes/modules/tasks/task.controller.js
@@ -0,0 +1,46 @@
+const {ResData} = require("../../../lib/resData.js")
+
+class UserController{
+    #service
+    constructor(service){
+        this.#service = service;
+    }
+    
+    async getAllUsers(req,res){
+        try{
+            return await this.#service.getAllUsers()
+        }catch(err){
+            console.log(err);
+            return err;
+        }
+    }
+
+    async register(req,res){
+        const dto = req.body;
+        try{
+            if(!dto.fullName || !dto.gmail || !dto.password){
+                return new ResData("fullName, gmail, password must be require")
+            }
+            return await this.#service.registerService(dto, req,res);
+            
+        }catch(err){
+            console.log(err);
+            return err;
+        }
+    }
+
+    async login(req,res){
+        try{
+            const dto = req.body;
+            if(!dto.gmail || !dto.password){
+                return new ResData("gmail and password must be required")
+            }
+            return await this.#service.loginService(dto, req,res)
+        }catch(err){
+            console.log(err);
+            return err;
+        }
+    }
+}
+
+module.exports = {UserController};
---
+++ src/routes/modules/tasks/task.service.js
@@ -0,0 +1,51 @@
+const {SqlService} = require("../../../database/service_db.js");
+const {ResData} = require("../../../lib/resData.js");
+const {getToken, verifyToken} = require("../../../lib/jwt.js")
+const {hashPasword, verifyPassword} = require("../../../lib/bcrypt.js");
+
+
+class UserService{
+    async getAllUsers(){
+        const sqlService = new SqlService();
+        const data = await sqlService.getAllUsers();
+
+        return new ResData("All users", 200, data);
+    }
+
+    async registerService(dto,req,res){
+        try{
+
+            dto.password = await hashPasword(dto.password);
+
+            const sqlService = new SqlService();
+            const data = await sqlService.createNewUser(dto);
+            const token = await getToken(...data.rows)
+            // await res.headers.set("token", token)
+            res.setHeader('token', token);
+            return new ResData("User created", 201, data.rows);
+        }catch(err){
+            console.log(err);
+            return new ResData(err.message || "something went wrong", 500, null, err);
+        }
+    }
+    async loginService(dto, req,res){
+        try{
+            const sqlService = new SqlService();
+
+            const currentUser = await sqlService.getOneByGmail(dto.gmail);
+            const user = currentUser.rows[0];
+            const checkLogin = await verifyPassword(dto.password, user.password);
+            if(!checkLogin){
+                return new ResData("Wrong password", 403)
+            }
+            const token = await getToken(user);
+            res.setHeader('token', token);
+            return new ResData("You successfully loged in", 200, user);
+        }catch(err){
+            console.log(err);
+            return new ResData(err.message || "something went wrong", 500, null, err);
+        }
+    }
+}
+
+module.exports = {UserService};
